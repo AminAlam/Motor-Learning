87fc66f1a2dccaa955b65aadcb6f86fdde5ed743
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-06-16 11:06:18 +0430

    Figre 4 mistakes are corrected

--- Simulations/Simulations of Motor Adaptation with the Multi-Rate Model Explain a Variety of Previously Reported Results, Including Rapid Unlearning and Rapid Downscaling/main.m
+++ Simulations/Simulations of Motor Adaptation with the Multi-Rate Model Explain a Variety of Previously Reported Results, Including Rapid Unlearning and Rapid Downscaling/main.m
@@ -54,7 +54,7 @@ box = [deadaptation_trials(1)-1 deadaptation_trials(1)-1 ...
 patch(box,boxy,'r','FaceAlpha',0.1, 'EdgeAlpha', 0)
 plot(1:num_trials_init, f, 'k', 'LineWidth', 4)
 ylim([-1.1 1.1])
-% xlim([1, 150])
+xlim([1, 150])
 
 figure(2)
 yline(0, '--k');
@@ -67,7 +67,7 @@ hold on
 counter = 1;
 for length_deadaptation_trials = [30 60 120]*2.5
     deadaptation_trials = 40+length_deadaptation_trials:num_trials_init;
-    num_trials = num_trials_init; %deadaptation_trials(end);
+    num_trials = num_trials_init;
     washout_trials = [];
     f = make_disturbance(num_trials, deadaptation_trials, washout_trials,...
                         length_initial_zeros);
@@ -92,9 +92,6 @@ for length_deadaptation_trials = [30 60 120]*2.5
     figure(3)
     x_readaptation =  x(40+length_deadaptation_trials+1:num_trials);
 
-%     x_readaptation = x_readaptation-min(x_readaptation);
-%     x_readaptation = x_readaptation/max(x_readaptation);
-% 	x_readaptation = x_readaptation*max(x_init);
     x_readaptation = -1*x_readaptation;
     [M,I] = min(abs(x_readaptation));
     plot(x_readaptation(I:end), color, 'LineWidth', 2, 'LineStyle', '--')
@@ -104,7 +101,6 @@ end
 
 figure(2)
 plot(1:num_trials_init, x_init, 'b', 'LineWidth', 2)
-% xlim([0 4])
 ylim([-0.7 0.7])
 
 figure(3)
@@ -114,23 +110,20 @@ xlim([0 400])
 clc
 close all
 
-deadaptation_trials = 101:num_trials_init;
-washout_trials = [];
+deadaptation_trials = [];
+washout_trials = 100:num_trials_init;
 f = make_disturbance(num_trials_init, deadaptation_trials, washout_trials,...
                     length_initial_zeros);
 
-figure(1)
+figure
 hold on
 boxy = [-1.1 1.1 1.1 -1.1];
 box = [length_initial_zeros length_initial_zeros ...
-        deadaptation_trials(1)-1 deadaptation_trials(1)-1];
+        washout_trials(1)-1 washout_trials(1)-1];
 patch(box,boxy,'k','FaceAlpha',0.1, 'EdgeAlpha', 0)
-box = [deadaptation_trials(1)-1 deadaptation_trials(1)-1 ...
-        deadaptation_trials(end) deadaptation_trials(end)];
-patch(box,boxy,'r','FaceAlpha',0.1, 'EdgeAlpha', 0)
 plot(1:num_trials_init, f, 'k', 'LineWidth', 4)
 ylim([-1.1 1.1])
-% xlim([1, 150])
+xlim([1, 150])
 
 figure(2)
 yline(0, '--k');
@@ -143,7 +136,7 @@ hold on
 counter = 1;
 for length_deadaptation_trials = [30 60 120]*2.5
     washout_trials = 40+length_deadaptation_trials:num_trials_init;
-    num_trials = num_trials_init; %deadaptation_trials(end);
+    num_trials = num_trials_init;
     deadaptation_trials = [];
     f = make_disturbance(num_trials, deadaptation_trials, washout_trials,...
                         length_initial_zeros);
@@ -168,10 +161,6 @@ for length_deadaptation_trials = [30 60 120]*2.5
     figure(3)
     x_readaptation =  x(40+length_deadaptation_trials+1:num_trials);
 
-%     x_readaptation = x_readaptation-min(x_readaptation);
-%     x_readaptation = x_readaptation/max(x_readaptation);
-% 	x_readaptation = x_readaptation*max(x_init);
-    [M,I] = min(abs(x_readaptation));
     [~, I] = min(abs(x_readaptation-0.4));
     x_readaptation = -1*x_readaptation;
     x_readaptation = x_readaptation+0.4;
@@ -183,33 +172,33 @@ end
 
 figure(2)
 plot(1:num_trials_init, x_init, 'b', 'LineWidth', 2)
-% xlim([0 4])
 ylim([-0.7 0.7])
 
 figure(3)
 plot(1:num_trials_init-length_initial_zeros, x_init(length_initial_zeros+1:num_trials_init), 'b', 'LineWidth', 2)
 xlim([0 400])
-%% Paradigm 3-test
+%% Paradigm 3
 clc
 close all
 
-deadaptation_trials = 101:num_trials_init;
-washout_trials = [];
+deadaptation_trials = [];
+washout_trials = 100:num_trials_init;
 f = make_disturbance(num_trials_init, deadaptation_trials, washout_trials,...
                     length_initial_zeros);
+f(100:num_trials_init) = 0.5;         
 
-figure(1)
+figure
 hold on
 boxy = [-1.1 1.1 1.1 -1.1];
 box = [length_initial_zeros length_initial_zeros ...
-        deadaptation_trials(1)-1 deadaptation_trials(1)-1];
+        washout_trials(1)-1 washout_trials(1)-1];
 patch(box,boxy,'k','FaceAlpha',0.1, 'EdgeAlpha', 0)
-box = [deadaptation_trials(1)-1 deadaptation_trials(1)-1 ...
-        deadaptation_trials(end) deadaptation_trials(end)];
-patch(box,boxy,'r','FaceAlpha',0.1, 'EdgeAlpha', 0)
+box = [washout_trials(1)-1 washout_trials(1)-1 ...
+        num_trials_init num_trials_init];
+patch(box,boxy,'p','FaceAlpha',0.1, 'EdgeAlpha', 0)
 plot(1:num_trials_init, f, 'k', 'LineWidth', 4)
 ylim([-1.1 1.1])
-% xlim([1, 150])
+xlim([1, 150])
 
 figure(2)
 yline(0, '--k');
@@ -250,10 +239,6 @@ for length_deadaptation_trials = [30 60 120]*2.5
     figure(3)
     x_readaptation =  x(40+length_deadaptation_trials+1:num_trials);
 
-%     x_readaptation = x_readaptation-min(x_readaptation);
-%     x_readaptation = x_readaptation/max(x_readaptation);
-% 	x_readaptation = x_readaptation*max(x_init);
-    [M, I] = min(abs(x_readaptation));
     [~, I] = min(abs(x_readaptation-0.4));
     x_readaptation = -1*x_readaptation;
     x_readaptation = x_readaptation+0.4;
@@ -266,128 +251,8 @@ end
 
 figure(2)
 plot(1:num_trials_init, x_init, 'b', 'LineWidth', 2)
-% xlim([0 4])
 ylim([-0.7 0.7])
 
 figure(3)
 plot(1:num_trials_init-length_initial_zeros, x_init(length_initial_zeros+1:num_trials_init), 'b', 'LineWidth', 2)
 xlim([0 400])
-%% Paradigm 2
-clc
-close all
-
-deadaptation_trials = [];
-washout_trials = 100:num_trials_init;
-f = make_disturbance(num_trials_init, deadaptation_trials, washout_trials,...
-                    length_initial_zeros);
-
-figure
-hold on
-boxy = [-1.1 1.1 1.1 -1.1];
-box = [length_initial_zeros length_initial_zeros ...
-        washout_trials(1)-1 washout_trials(1)-1];
-patch(box,boxy,'k','FaceAlpha',0.1, 'EdgeAlpha', 0)
-plot(1:num_trials_init, f, 'k', 'LineWidth', 4)
-ylim([-1.1 1.1])
-xlim([1, 150])
-
-figure
-yline(0, '--k');
-hold on
-counter = 1;
-for length_deadaptation_trials = [30 60 120]
-    deadaptation_trials = [];
-    washout_trials = 40+length_deadaptation_trials:40+length_deadaptation_trials+50;
-    num_trials = washout_trials(end);
-    f = make_disturbance(num_trials, deadaptation_trials, washout_trials,...
-                        length_initial_zeros);
-    x = zeros(1, num_trials);
-    x1 = zeros(1, num_trials);
-    x2 = zeros(1, num_trials);
-    for trial_no = 2:num_trials
-        if ~isempty(find(washout_trials==trial_no, 1))
-            err_clamp_bool = 1;
-        else
-            err_clamp_bool = 0;
-        end
-        [x1(trial_no), x2(trial_no), x(trial_no)] = multi_rate(...
-            x1(trial_no-1), x2(trial_no-1), f(trial_no-1), [Af, As], [Bf, Bs], err_clamp_bool);
-    end
-    
-    if counter == 1
-        color = 'r';
-    elseif counter == 2
-        color = 'g';
-    else
-        color = 'c';
-    end
-    plot(1:num_trials, x, color, 'LineWidth', 2, 'LineStyle', '--')
-    counter = counter+1;
-end
-
-plot(1:num_trials_init, x_init, 'b', 'LineWidth', 2)
-xlim([0 210])
-ylim([-0.7 0.7])
-
-%% Paradigm 3
-clc
-close all
-
-deadaptation_trials = [];
-washout_trials = 100:num_trials_init;
-f = make_disturbance(num_trials_init, deadaptation_trials, washout_trials,...
-                    length_initial_zeros);
-f(100:num_trials_init) = 0.5;         
-
-figure
-hold on
-boxy = [-1.1 1.1 1.1 -1.1];
-box = [length_initial_zeros length_initial_zeros ...
-        washout_trials(1)-1 washout_trials(1)-1];
-patch(box,boxy,'k','FaceAlpha',0.1, 'EdgeAlpha', 0)
-box = [washout_trials(1)-1 washout_trials(1)-1 ...
-        num_trials_init num_trials_init];
-patch(box,boxy,'p','FaceAlpha',0.1, 'EdgeAlpha', 0)
-plot(1:num_trials_init, f, 'k', 'LineWidth', 4)
-ylim([-1.1 1.1])
-xlim([1, 150])
-
-figure
-yline(0, '--k');
-hold on
-counter = 1;
-for length_deadaptation_trials = [30 60 120]
-    deadaptation_trials = [];
-    washout_trials = [];
-    num_trials = 40+length_deadaptation_trials+40;
-    f = make_disturbance(num_trials, deadaptation_trials, washout_trials,...
-                        length_initial_zeros);
-    f(40+length_deadaptation_trials:end) = 0.5;
-    x = zeros(1, num_trials);
-    x1 = zeros(1, num_trials);
-    x2 = zeros(1, num_trials);
-    for trial_no = 2:num_trials
-        if ~isempty(find(washout_trials==trial_no, 1))
-            err_clamp_bool = 1;
-        else
-            err_clamp_bool = 0;
-        end
-        [x1(trial_no), x2(trial_no), x(trial_no)] = multi_rate(...
-            x1(trial_no-1), x2(trial_no-1), f(trial_no-1), [Af, As], [Bf, Bs], err_clamp_bool);
-    end
-    
-    if counter == 1
-        color = 'r';
-    elseif counter == 2
-        color = 'g';
-    else
-        color = 'c';
-    end
-    plot(1:num_trials, x, color, 'LineWidth', 2, 'LineStyle', '--')
-    counter = counter+1;
-end
-
-plot(1:num_trials_init, x_init, 'b', 'LineWidth', 2)
-xlim([0 210])
-ylim([-0.7 0.7])
-
